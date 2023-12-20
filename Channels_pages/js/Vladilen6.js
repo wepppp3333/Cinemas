9e268972980106fa34e414ddb0ef3ac48d36c342
ILYA <fadinarzamas@mail.ru>
2023-12-20 17:18:02 +0300

    20.12.2023

--- Channels_pages/js/Vladilen6.js
+++ Channels_pages/js/Vladilen6.js
@@ -8,47 +8,53 @@
 // console.log(json);
 // console.log(parsed);
 
-const list = document.querySelector('#list')
-const filter = document.querySelector('#filter')
-let USERS = []
+const list = document.querySelector("#list");
+const filter = document.querySelector("#filter");
+let USERS = [];
 
-
-filter.addEventListener('input',(event) => {
-  const value = event.target.value.toLowerCase()
+filter.addEventListener("input", (event) => {
+  const value = event.target.value.toLowerCase();
   const filteredUsers = USERS.filter((user) => {
-    return user.name.toLowerCase().includes(value)
-  })
-  render(filteredUsers)
-})
-
+    return user.name.toLowerCase().includes(value);
+  });
+  render(filteredUsers);
+});
 
 async function start() {
-  list.innerHTML = 'Loadding...'
+  list.innerHTML = "Loadding...";
   try {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await resp.json()
+    const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+    console.log(resp);
+    const data = await resp.json();
+    console.log(data);
     setTimeout(() => {
-      USERS = data
-      render(data)
-    },2000)
+      USERS = data;
+      render(data);
+    }, 2000);
   } catch (err) {
-    list.style.color = 'red'
-    list.innerHTML = err.message
+    list.style.color = "red";
+    list.innerHTML = err.message;
   }
 }
 
-function render (users = []) {
-  if(users.length === 0) {
-    list.innerHTML = 'No matched users!'
+function render(users = []) {
+  if (users.length === 0) {
+    list.innerHTML = "No matched users!";
   } else {
-    const html = users.map(toHTML).join('')
-    list.innerHTML = html
+    const html = users.map(toHTML).join("");
+    list.innerHTML = html;
   }
 }
 
 function toHTML(user) {
   return `
-  <li class="list-group-item">${user.name}</li>
-  `
+  <table>
+    <tr>
+      <td><li class="list-group-item">${user.name}</li>
+      </td>
+      <td><li class="list-group-item">${user.email}</li></td>
+    </tr>
+  </table>
+  `;
 }
 start();

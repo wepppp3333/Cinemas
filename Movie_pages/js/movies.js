f42082c45bce7f083308de388c0ffb280a5963d7
wepppp3333 <fadinarzamas@mail.ru>
2024-01-26 22:09:13 +0300

    27.01.2024 HOME-PC

--- Movie_pages/js/movies.js
+++ Movie_pages/js/movies.js
@@ -1,7 +1,6 @@
 const listFilms = document.querySelector(".new_movies__card");
 const btn_continuation = document.querySelector(".container__btn_continuation");
-const COUNT_SHOW_CARDS_FILM = 10;
-
+const COUNT_SHOW_CARDS_FILM = 7;
 let filmData = [];
 
 getFilms();
@@ -46,14 +45,17 @@ function createCardsFilms(data) {
   data.forEach((card) => {
     const { id, bacgroundImage, title } = card;
     const cardFilmsItem = `
-    <div class="card_rectangle_2 card_rectangle movies__container_card_hover" style="background-image: url('${bacgroundImage}');">
-        <div class="btn_play_cinemas" href="/index.html">
-          <a href="/Movie_pages/movies_cards/card_film.html?id=${id}">
-            <img src="/Movie_pages/Image/play_twitter_video_icon_127120.svg" alt="" class="btn_play_cinemas_img">
-          </a>
-        </div>
-      </div>
+<div class="card_rectangle_2 card_rectangle movies__container_card_hover" style="background-image: url('${bacgroundImage}');">
+    <div class="btn_play_cinemas" href="/index.html">
+      <a href="/Movie_pages/movies_cards/card_film.html?id=${id}">
+        <img src="/Movie_pages/Image/play_twitter_video_icon_127120.svg" alt="" class="btn_play_cinemas_img">
+      </a>
+    </div>
+  </div>
     `;
     listFilms.insertAdjacentHTML("beforeend", cardFilmsItem);
   });
 }
+
+
+
